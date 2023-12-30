9df88d6b75eb0b70ee20b5de095c371fda04cba6
Rachel <133939424+rxchell@users.noreply.github.com>
2023-12-30 14:52:50 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function MyButton() {
+  return (
+    <button>
+      Login
+    </button>
+  );
+}
+
+export function MyApp() {
+  return (
+    <div>
+      <h1>e-commerce</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Welcome to </h2>
+      <MyApp />
     </div>
   );
 }
