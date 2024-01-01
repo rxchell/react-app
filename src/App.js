9c61cead7516c95c4abc358c7b3bb9faf30172f8
Rachel <133939424+rxchell@users.noreply.github.com>
2024-01-01 11:53:03 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import './App.css';
+import React from 'react';
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Categories } from './pages/Categories';
+import Login from './pages/Login';
+import { Notfound } from './pages/Notfound';
+import { CategoriesLayout } from './CategoriesLayout';
+import "./styles.css";
 
-function MyButton() {
+function App() {
   return (
-    <button>
-      Login
-    </button>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              style={({ isActive }) => {
+                return isActive ? { color: 'blue' } : { color: 'black' };
+              }}
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories"replace>Categories</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<CategoriesLayout  />}>
+          <Route path=":id" element={<Categories />} />
+          <Route path=":id/:subcategory" element={<Categories />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
   );
 }
 
-export function MyApp() {
-  return (
-    <div>
-      <h1>e-commerce</h1>
-      <MyButton />
-    </div>
-  );
-}
-
-const App = () => {
-  return (
-    <div className="App">
-      <h2> Welcome to </h2>
-      <MyApp />
-    </div>
-  );
-}
-
-export default App;
+export default App
